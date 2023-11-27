e96032fda823ee883458a1d53ecbf2d2ecb0d4c2
jakemoss127 <jacobmoss@ufl.edu>
2023-11-27 17:45:12 -0500

    Added role dropdown during signup

--- client/src/views/CreateAccount/CreateUser.jsx
+++ client/src/views/CreateAccount/CreateUser.jsx
@@ -2,9 +2,7 @@ import React, { useState } from "react";
 import "./CreateUserPage.less";
 import { createUser, getStudentClassroom } from "../../Utils/requests";
 import { message } from "antd";
-import { makeRequest } from "../../Utils/requests.js";
-import { useNavigate } from "react-router-dom";
-import { setUserSession } from "../../Utils/AuthRequests";
+import { makeRequest } from '../../Utils/requests.js';
 // import {eachLimit} from "../../../public/lib/avrgirl-arduino.global";
 
 const CreateUser = () => {
@@ -13,6 +11,7 @@ const CreateUser = () => {
     const [email, setEmail] = useState('');
     const [username, setUsername] = useState(''); 
     const [password, setPassword] = useState('');
+    const [role, setRole] = useState(''); 
 
   const validateEmail = (emailInput) => {
     //validates email address
@@ -29,54 +28,47 @@ const CreateUser = () => {
     } else return false;
   };
 
-  //handler functions for emails / passwords
-  const handleEmailChange = (entry) => {
-    setEmail(entry.target.value);
-  };
+    //handler functions for emails / passwords
+    const handleEmailChange = (entry) => {
+        setEmail(entry.target.value);
+    }
 
-  const handleUsernameChange = (entry) => {
-    setUsername(entry.target.value);
-  };
+    const handleUsernameChange = (entry) => {
+        setUsername(entry.target.value);
+    }
 
   const handlePasswordChange = (entry) => {
     setPassword(entry.target.value);
   };
 
-  const createUserFunction = async (e) => {
-    e.preventDefault();
+  const createUserFunction = async () => {
     if (validateEmail(email) && validatePassword(password)) {
       const runRequest = async () => {
         try {
-          const res = await createUser(username, email, password, "student");
+          console.log("Trying to createUser function"); 
+          const res = await createUser(userId, email, password);
           if (res.data) {
             if (res.data.messages) {
               message.error(res.data.messages);
             } else {
-              message.info("User created");
-              // setUsername("");
-              // setEmail("");
-              // setPassword("");
-
-              //Login and navigate to the correct page
-              setUserSession(res.data.jwt, JSON.stringify(res.data.user));
-
-              if (res.data.user.role.name === "Content Creator") {
-                navigate("/ccdashboard");
-              } else if (res.data.user.role.name === "Researcher") {
-                navigate("/report");
-              } else {
-                navigate("/dashboard");
-              }
+              message.info("User created: " + userId);
             }
           } else {
             message.error(res.err);
           }
-          console.log("Finished function");
+          console.log("Finished function"); 
         } catch (err) {
           console.log("Some error happened: " + err);
         }
       };
       await runRequest();
+
+      /*try {
+                // Create user here using SQL
+                const response = await fetch(''); 
+            } catch(error) { //Catch any error and log to the screen
+                console.error('Error: ', error); 
+            }*/
     } else alert("Invalid email or password!");
   };
 
@@ -100,8 +92,13 @@ const CreateUser = () => {
                 value={password}
                 onChange={handlePasswordChange}
             />
+            <h2 className="role-drop-title" for="role-names">Choose Role</h2>
+            <select name="role-names" id="role-names"
+            onChange={(e) => setRole(e.target.value)}>
+              <option value="student">Student</option> 
+              <option value="teacher">Teacher</option>
+            </select>
             <button className='route-button' onClick={createUserFunction}>Create Account</button>
-        
     </div>
   );
 };
