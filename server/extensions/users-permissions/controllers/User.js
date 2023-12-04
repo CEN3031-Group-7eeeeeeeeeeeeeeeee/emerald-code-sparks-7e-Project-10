79eb60f9fefc5da961b3678d4e3f67d1553f90bf
Samson <simhonsamson@gmail.com>
2023-12-04 13:25:09 -0500

    Added createUser request calling the API endpoint /auth/local/register
    Not tested yet

--- server/extensions/users-permissions/controllers/User.js
+++ server/extensions/users-permissions/controllers/User.js
@@ -41,6 +41,7 @@ module.exports = {
       return (ctx.response.status = 401);
     }
 
+
     const newUser = { ...ctx.request.body };
     if ('password' in newUser && newUser.password) {
       newUser.password = await strapi.plugins["users-permissions"].services.user.hashPassword(newUser.password);
